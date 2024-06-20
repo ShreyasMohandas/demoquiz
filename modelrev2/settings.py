969adde0a2e7914c1b694db701d3827eb2ae0bd2
Shreyas <shreyasmedu@gmail.com>
2024-06-20 19:17:48 +0530

    Include build steps in Python environment

--- modelrev2/settings.py
+++ modelrev2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5og0jurl6lcw-*yhtfo%u9d3)4smu)2=hw9g+l4w!9g#^vk0$o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
