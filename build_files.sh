8724b6fbbb89f82676c1d5d8b84724cd890c59d1
Shreyas <shreyasmedu@gmail.com>
2024-06-20 18:31:38 +0530

    vercel commit

---
+++ build_files.sh
@@ -0,0 +1,2 @@
+python manage.py collectstatic
+pip install -r requirements.txt 
--- modelrev2/settings.py
+++ modelrev2/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'modelrev2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gfcoNJWPeptyDnGBSMKsfKChXITApyxg',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '29949',
     }
 }
+
 
 
 # Password validation
@@ -117,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
